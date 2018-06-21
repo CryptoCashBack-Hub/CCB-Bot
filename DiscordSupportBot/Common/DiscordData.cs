fedc21298711796b6b47c08ce22b53d72789fdcd
Tfinch <travisfinch01@gmail.com>
2018-06-21 13:14:04 -0400

    Channel fix

--- DiscordSupportBot/Common/DiscordData.cs
+++ DiscordSupportBot/Common/DiscordData.cs
@@ -4,13 +4,8 @@ namespace DiscordSupportBot.Common
 {
     public static class DiscordData
     {
-<<<<<<< HEAD
-        public static ulong BotChannel { get => 454464504290476033; }
         public static ulong BotPollChannel { get => 459355715954278400; }
-=======
         public static ulong BotChannel { get => 458762234516996116; }
-
->>>>>>> ca7e49414bcbd4623dd36ee25a754da8a9e4dcfc
         public static ulong BotTestingChannel { get => 457926138010927105; }
         
     }
